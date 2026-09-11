2b8333044e68a1b456a85f8ffea6434e6215b402
Durel100 <fouedjeudurel02@gmail.com>
2026-09-11 11:32:25 +0100

    fix budget

--- app/Models/CategoryBudget.php
+++ app/Models/CategoryBudget.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class CategoryBudget extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'monthly_budget',
+    ];
+
+    protected $casts = [
+        'monthly_budget' => 'decimal:2',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
